a0ffd81f7aca26a6023943b4a06282601b7c99ed
Fulltea <rikuta@furutan.com>
2023-10-24 20:00:40 +0900

    edit

--- src/access.py
+++ src/access.py
@@ -2,6 +2,8 @@ from utils import *
 from tqdm.auto import tqdm
 import itertools
 import os 
+import time
+
 
 conn = getConn()
 M = conn.getblockcount()
@@ -16,10 +18,13 @@ for height in tqdm(range(1, M)):
             transactionHex = getTransactionHex(transactionId)
             try:
                 results += getSignAndPubkeys(transactionHex)
+                time.sleep(0.1)
             except:
                 pass
-    except:
-        pass
+        time.sleep(0.1)
+    except Exception as e:
+        print(e)
+        
 
 file_name = os.environ["RPC_HOST"]+".txt"
 with open(os.path.join("/mnt",file_name), "w") as fp:
