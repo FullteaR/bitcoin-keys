38bc6df985f2449107a42f8720bcde96d7bf6c16
Fulltea <rikuta@furutan.com>
2023-10-27 10:39:30 +0900

    edit: add height to outputwq

--- src/access.py
+++ src/access.py
@@ -10,17 +10,19 @@ time.sleep(30)
 conn = getConn()
 M = conn.getblockcount()
 
-results = []
+results = {}
 
 for height in tqdm(range(1, M)):
+    results[height] = []
     try:
         blockHash = getBlockHash(height, conn)
         transactionIds = getTransactionIds(blockHash, conn)
         for transactionId in transactionIds:
             transactionHex = getTransactionHex(transactionId, conn)
             try:
-                results += getSignAndPubkeys(transactionHex)
-            except:
+                results[height] += getSignAndPubkeys(transactionHex)
+            except Exception as ex:
+                print(ex)
                 pass
         time.sleep(0.1)
     except Exception as e:
@@ -29,8 +31,9 @@ for height in tqdm(range(1, M)):
 
 file_name = os.environ["RPC_HOST"]+".txt"
 with open(os.path.join("/mnt",file_name), "w") as fp:
-    for result in results:
-        r,s = result[0]
-        x,y = result[1]
-        fp.write(f"{r},{s},{x},{y}\n")
+    for height in range(1, M):
+        for result in results[height]:
+            r,s = result[0]
+            x,y = result[1]
+            fp.write(f"{height},{r},{s},{x},{y}\n")
 
